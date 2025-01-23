4783b522a37eeac3f67c77acac0906d79ea61f3a
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-01-24 01:29:06 +0600

    change

--- JavaScript/day2/condition.js
+++ JavaScript/day2/condition.js
@@ -1,5 +1,7 @@
 console.log('hello i am learn javascript tutorial');
 
 let x = 44;
-if (x < 50) console.log('accepted');
-else console.log('not accepted');
+let y = 77;
+// if (x < 50) console.log('accepted');
+// else console.log('not accepted');
+let c = x > y ? x - y : y - x;
