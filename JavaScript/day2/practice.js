93dddd217b750c9491700b30f1c1f0da4b8a01e1
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-01-24 00:08:37 +0600

    learn variables

---
+++ JavaScript/day2/practice.js
@@ -0,0 +1,33 @@
+// // var a = 6;
+// // var b = 7;
+// // var c = 'JavaScript';
+
+// // console.log(a + b + 8);
+// // console.log(typeof a, typeof b, typeof c);
+
+// let x = 6;
+// let y = 6;
+
+// {
+//   let x = 77;
+//   console.log(x);
+// }
+
+// console.log(x);
+let x = 'Harry vai';
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(x, y, z, p, q, r);
+
+let o = {
+  name: 'harry',
+  'job code': 5600,
+  is_handsome: true,
+};
+console.log(o);
+o.salary = 10000;
+console.log(o);
