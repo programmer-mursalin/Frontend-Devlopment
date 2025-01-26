21d0c1bd80aabf26f99b746f8315797ce50e9ff4
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-01-26 20:30:30 +0600

    function

---
+++ JavaScript/day3/function.js
@@ -0,0 +1,13 @@
+function nice(name) {
+  console.log('hey ' + name + 'you are nice');
+  console.log('hey ' + name + 'you are nice');
+  console.log('hey ' + name + 'you are nice');
+  console.log('hey ' + name + 'you are nice');
+  console.log('hey ' + name + 'you are nice');
+}
+nice('rohan');
+
+const func1 = (x) => {
+  console.log('I am a arrow function', x);
+};
+func1(34);
