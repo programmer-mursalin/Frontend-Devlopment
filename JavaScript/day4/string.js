d6f691e8869189a709d538cdb8b5695200c1f04f
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-01-31 03:58:48 +0600

    string

---
+++ JavaScript/day4/string.js
@@ -0,0 +1,20 @@
+// console.log('hey I am a javascript tutorial');
+let a = 'harray';
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// let name1 = 'harry';
+// let friend = 'rohan';
+// abcdefghijklmnopqrstuvwxyz;
+// console.log('   his name is  ' + name1 + '  his friend is  ' + friend);
+// console.log(`his name is ${name1}  and his friend  name is ${friend}`);
+let c = 'mursalin';
+// console.log(c.toUpperCase()); // This is correct JavaScript syntax
+// console.log(c.slice(1, 5)); // sub string with specific index
+// console.log(c.replace('mur', '88'));
+// console.log(c.concat(a));
+// console.log(c.concat(a, '  akib', ' mridul', 'rajib'));
+let v = c.concat(a, ' akib', 'mridul', 'rajib');
+console.log(c);
+console.log(v);
