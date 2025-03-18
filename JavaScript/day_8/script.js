62427e74a1a2fd4cb533f924f5a98c758c3119cf
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-03-18 23:57:04 +0600

    dom_insert

---
+++ JavaScript/day_8/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.box').innerHTML = 'hey i am shuvo'; // change html into box div
+document.querySelector('.box').hasAttribute('style'); // return true if style tag in particular tag
+document.querySelector('.box').getAttribute('style'); // return this attribute
+document.querySelector('.box').setAttribute('style', 'display:inline'); // set new attribute
+document.querySelector('.box').attributes;
+document.querySelector('.box').removeAttribute('style');
+//document.designMode = 'on'; // any edit one in website
+
+document.querySelector('. box').dataset;
