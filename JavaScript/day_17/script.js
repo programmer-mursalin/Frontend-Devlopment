1ddd0b4b064013db429f7c41c18553e321ac15d4
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-04-26 02:15:55 +0600

    error

---
+++ JavaScript/day_17/script.js
@@ -0,0 +1,7 @@
+let a = prompt('Enter  the fast number');
+let b = prompt('Enter  the second number');
+
+if (isNaN(a) || isNaN(b)) {
+  throw SyntaxError('Sorry this is not allowed');
+}
+let sum = parseInt(a) + parseInt();
